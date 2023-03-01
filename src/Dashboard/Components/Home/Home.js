ccb456f1842ca1e9e790d8faebef6ee7ad32a480
Amit Sarker <amitsarker255@gmail.com>
2023-03-01 23:06:18 +0600

    history added

--- src/Dashboard/Components/Home/Home.js
+++ src/Dashboard/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Date from "../Date/Date";
+import History from "../History/History";
 
 const Home = () => {
   return (
     <div>
       <Date></Date>
+      <History></History>
     </div>
   );
 };
